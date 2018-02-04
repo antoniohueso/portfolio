26ccb0582bc692728c010ae7e2945c9d49abfa8d
ahg <antonio.hueso@gmail.com>
2018-02-05 00:10:21 +0100

    spring data

---
+++ src/main/java/es/corpme/sc/portfolio/ExposeEntityIdRestConfiguration.java
@@ -0,0 +1,19 @@
+package es.corpme.sc.portfolio;
+
+import es.corpme.sc.portfolio.entity.Desarrollo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+
+/**
+ * Created by ahg on 5/2/18.
+ */
+@Configuration
+public class ExposeEntityIdRestConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Desarrollo.class);
+    }
+}
--- src/main/java/es/corpme/sc/portfolio/repository/DesarrolloRepository.java
+++ src/main/java/es/corpme/sc/portfolio/repository/DesarrolloRepository.java
@@ -2,8 +2,9 @@ package es.corpme.sc.portfolio.repository;
 
 import es.corpme.sc.portfolio.entity.Desarrollo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "desarrollo", path = "desarrollo")
 public interface DesarrolloRepository extends MongoRepository<Desarrollo, String> {
 }
